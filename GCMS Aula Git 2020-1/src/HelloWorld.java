09903b449fef1bb4fa0dd3e77418e87a4574e25a
Heber Leandro <heberleandroluz@gmail.com>
2020-09-16 19:58:17 -0300

    commit heber.

--- GCMS Aula Git 2020-1/src/HelloWorld.java
+++ GCMS Aula Git 2020-1/src/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!! - BASH/Eclipse");
 		System.out.println("Novo commit: BASH + Eclipse");
+
+		System.out.println("heber commit.");
 	}
 
 }
