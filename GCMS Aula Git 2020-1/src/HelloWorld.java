84e25b0ff516ee0f303a28a67a64845c384bd99e
Jerônimo Correia de Morais <53715463+JeraDeveloppeur@users.noreply.github.com>
2020-09-21 13:34:17 -0300

    Inseri o meu sobrenome.

--- GCMS Aula Git 2020-1/src/HelloWorld.java
+++ GCMS Aula Git 2020-1/src/HelloWorld.java
@@ -4,6 +4,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!! - BASH/Eclipse");
 		System.out.println("Novo commit: BASH + Eclipse");
+		System.out.println("Meu nome é Jerônimo Morais.");
+		System.out.println("Eu estou estudando GCMS.");
+		System.out.println("Estamos retomando as aulas de forma remota.");
 	}
 
 }
