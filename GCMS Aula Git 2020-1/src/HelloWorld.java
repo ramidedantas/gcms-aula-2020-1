6b8152700503bebb625536173aa0dbd44b9ec52a
melo <melo@I-N4050>
2020-09-21 12:56:00 -0300

    Leandro Melo Commit

--- GCMS Aula Git 2020-1/src/HelloWorld.java
+++ GCMS Aula Git 2020-1/src/HelloWorld.java
@@ -4,7 +4,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!! - BASH/Eclipse");
 		System.out.println("Novo commit: BASH + Eclipse");
+
 		System.out.println("Leandro S Melo");
+
+
 	}
 
 }
