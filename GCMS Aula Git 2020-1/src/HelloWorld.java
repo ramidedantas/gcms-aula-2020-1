e24023c1c499e3426dd69955f2bb3127a69d0929
Jerônimo Correia de Morais <53715463+JeraDeveloppeur@users.noreply.github.com>
2020-09-21 13:34:37 -0300

    Inseri o nome do professor da disciplina.

--- GCMS Aula Git 2020-1/src/HelloWorld.java
+++ GCMS Aula Git 2020-1/src/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 		System.out.println("Hello World!!! - BASH/Eclipse");
 		System.out.println("Novo commit: BASH + Eclipse");
 		System.out.println("Meu nome é Jerônimo Morais.");
-		System.out.println("Eu estou estudando GCMS.");
+		System.out.println("Eu estou estudando GCMS com o professor Ramide.");
 		System.out.println("Estamos retomando as aulas de forma remota.");
 	}
 
