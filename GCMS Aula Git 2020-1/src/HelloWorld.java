847580d1329d3dbbfb184cd1019711d7447759b3
Jerônimo Correia de Morais <53715463+JeraDeveloppeur@users.noreply.github.com>
2020-09-21 13:35:21 -0300

    Inseri o motivo da forma de retomada das aulas.

--- GCMS Aula Git 2020-1/src/HelloWorld.java
+++ GCMS Aula Git 2020-1/src/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 		System.out.println("Novo commit: BASH + Eclipse");
 		System.out.println("Meu nome é Jerônimo Morais.");
 		System.out.println("Eu estou estudando GCMS com o professor Ramide.");
-		System.out.println("Estamos retomando as aulas de forma remota.");
+		System.out.println("Estamos retomando as aulas de forma remota devido à pandemia do COVID-19.");
 	}
 
 }
