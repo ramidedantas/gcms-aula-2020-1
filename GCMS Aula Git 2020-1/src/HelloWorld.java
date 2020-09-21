b22152fc0ae0e2a994764e59bf0a7478770806d8
lsmads <lsm@a.recife.ifpe.edu.br>
2020-09-21 18:15:55 -0300

    commit leandro 5

--- GCMS Aula Git 2020-1/src/HelloWorld.java
+++ GCMS Aula Git 2020-1/src/HelloWorld.java
@@ -11,10 +11,10 @@ public class HelloWorld {
 		System.out.println("Meu nome � Jer�nimo Morais.");
 		System.out.println("Eu estou estudando GCMS com o professor Ramide.");
 		System.out.println("Estamos retomando as aulas de forma remota devido � pandemia do COVID-19.");
-		
 		System.out.println("Minha matr�cula no IFPE � 20182y6-rc0142");
 		System.out.println("Meu e-mail institucional agora � jcm1@discente.ifpe.edu.br");
-		System.out.println("Leandro S Melo");
+		
+		System.out.println("Leandro S Melo");		
 		System.out.println("pratica 2");
 
 	}
