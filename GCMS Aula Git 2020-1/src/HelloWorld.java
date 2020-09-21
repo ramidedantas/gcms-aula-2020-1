11cd9ca0459d7a6bd9b5fc8d7e8a19eb62c86211
Ramide Dantas <ramidedantas@recife.ufpe.edu.br>
2020-09-21 19:24:40 -0300

    Eclipse master 2

--- GCMS Aula Git 2020-1/src/HelloWorld.java
+++ GCMS Aula Git 2020-1/src/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 		System.out.println("Novo commit: BASH + Eclipse");
 		System.out.println("Leandro S Melo");
 
-		System.out.println("Eclipse master 1");
+		System.out.println("Eclipse master 2");
 
 		System.out.println("Meu nome � Jer�nimo Morais.");
 		System.out.println("Eu estou estudando GCMS com o professor Ramide.");
