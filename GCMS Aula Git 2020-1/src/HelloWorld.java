d4ff91ba75cfb1f778a4e324068dd019e5ba4067
Richard F Albor <rfa@a.recife.ifpe.edu.br>
2020-10-28 15:30:37 -0300

    pratica 03 passo 5 - Richard Ferreira Albor

--- GCMS Aula Git 2020-1/src/HelloWorld.java
+++ GCMS Aula Git 2020-1/src/HelloWorld.java
@@ -55,6 +55,7 @@ public class HelloWorld {
 		System.out.println("Jeronimo - pratica 03.");
 		System.out.println("O professor eh Ramide.");
 		System.out.println("Richard Albor - esta deveria ser a prática 02 não é mesmo?");
+		System.out.println("Richard Albor - esta deveria ser a prática 03 passo 5");
 	}
 
 }
